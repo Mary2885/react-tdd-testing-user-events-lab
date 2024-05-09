9511a20ba344150349a3711af5bab275bb6a2396
mary <Mariashee001@gmail.com>
2024-05-09 10:07:28 +0300

    complete

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import { useState } from 'react';
+
 function App() {
+  const [isReading, setIsReading] = useState(false);
+  const [isWatching, setIsWatching] = useState(false);
+  const [isSleeping, setIsSleeping] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function toggleReading(e) {
+    setIsReading(e.target.checked);
+  }
+
+  function toggleWatching(e) {
+    setIsWatching(e.target.checked);
+  }
+
+  function toggleSleeping(e) {
+    setIsSleeping(e.target.checked);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsSubmitted(true);
+  }
+
   return (
     <main>
-      <h1>Hi, I'm (your name)</h1>
-      <img alt="My profile pic" src="https://via.placeholder.com/350" />
+      <h1>Hi, I'm Benny</h1>
+      <h2>My profile pic</h2>
+      <img style={{ borderRadius: '50%' }} alt="My profile pic" src="https://avatars.githubusercontent.com/u/122904044?v=4" />
       <h2>About Me</h2>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
@@ -18,6 +45,23 @@ function App() {
         <a href="https://github.com">GitHub</a>
         <a href="https://linkedin.com">LinkedIn</a>
       </div>
+
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Enter name..." value={name} onChange={(e) => setName(e.target.value)} /> <br />
+        <input type="text" name="email" placeholder="Enter email..." value={email} onChange={(e) => setEmail(e.target.value)} />
+        <div>
+          <p>Select interests</p>
+          <input type="checkbox" id="reading" checked={isReading} onChange={toggleReading} />
+          <label htmlFor="reading">Reading</label> <br />
+          <input type='checkbox' id="watching" checked={isWatching} onChange={toggleWatching} />
+          <label htmlFor='watching'>Watching</label> <br />
+          <input type='checkbox' id='sleeping' checked={isSleeping} onChange={toggleSleeping} />
+          <label htmlFor="sleeping">Sleeping</label>
+        </div>
+        <button type='submit'>Submit</button>
+      </form>
+      
+      {isSubmitted && <p>Thank you for your subscription</p>}
     </main>
   );
 }
--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,91 +1,120 @@
 import { render, screen } from "@testing-library/react";
 import '@testing-library/jest-dom';
-
+import userEvent from '@testing-library/user-event'; 
 import App from "../App";
+
 
 // Portfolio Elements
 test("displays a top-level heading with the text `Hi, I'm _______`", () => {
   render(<App />);
-
   const topLevelHeading = screen.getByRole("heading", {
     name: /hi, i'm/i,
     exact: false,
     level: 1,
   });
-
   expect(topLevelHeading).toBeInTheDocument();
 });
 
 test("displays an image of yourself", () => {
   render(<App />);
-
   const image = screen.getByAltText("My profile pic");
-
-  expect(image).toHaveAttribute("src", "https://via.placeholder.com/350");
+  expect(image).toHaveAttribute("src", "https://avatars.githubusercontent.com/u/122904044?v=4");
 });
 
 test("displays second-level heading with the text `About Me`", () => {
   render(<App />);
-
   const secondLevelHeading = screen.getByRole("heading", {
     name: /about me/i,
     level: 2,
   });
-
   expect(secondLevelHeading).toBeInTheDocument();
 });
 
 test("displays a paragraph for your biography", () => {
   render(<App />);
-
   const bio = screen.getByText(/lorem ipsum/i);
-
   expect(bio).toBeInTheDocument();
 });
 
 test("displays the correct links", () => {
   render(<App />);
-
   const githubLink = screen.getByRole("link", {
     name: /github/i,
   });
   const linkedinLink = screen.getByRole("link", {
     name: /linkedin/i,
   });
-
-  expect(githubLink).toHaveAttribute(
-    "href",
-    expect.stringContaining("https://github.com")
-  );
-
-  expect(linkedinLink).toHaveAttribute(
-    "href",
-    expect.stringContaining("https://linkedin.com")
-  );
+  expect(githubLink).toHaveAttribute("href", expect.stringContaining("https://github.com"));
+  expect(linkedinLink).toHaveAttribute("href", expect.stringContaining("https://linkedin.com"));
 });
 
 // Newsletter Form - Initial State
 test("the form includes text inputs for name and email address", () => {
-  // your test code here
+  render(<App />);
+  const username = screen.getByPlaceholderText(/enter name.../i);
+  expect(username).toBeInTheDocument();
+  const email = screen.getByPlaceholderText(/enter email.../i);
+  expect(email).toBeInTheDocument();
 });
 
 test("the form includes three checkboxes to select areas of interest", () => {
-  // your test code here
+  render(<App />);
+  expect(screen.getByRole('checkbox',{name: /reading/i})).toBeInTheDocument();
+  expect(screen.getByRole('checkbox',{name: /Watching/i})).toBeInTheDocument();
+  expect(screen.getByRole('checkbox',{name: /Sleeping/i})).toBeInTheDocument();
 });
 
 test("the checkboxes are initially unchecked", () => {
-  // your test code here
+  render(<App />);
+  expect(screen.getByRole('checkbox',{name: /reading/i})).not.toBeChecked();
+  expect(screen.getByRole('checkbox',{name: /Watching/i})).not.toBeChecked();
+  expect(screen.getByRole('checkbox',{name: /Sleeping/i})).not.toBeChecked();
 });
 
 // Newsletter Form - Adding Responses
 test("the page shows information the user types into the name and email address form fields", () => {
-  // your test code here
+  render(<App />);
+  const nameInput = screen.getByPlaceholderText(/enter name.../i); 
+
+  userEvent.type(nameInput, 'Benny'); 
+  expect(nameInput).toHaveValue('Benny');
+
+  const emailInput = screen.getByPlaceholderText(/enter email.../i); 
+
+  userEvent.type(emailInput, 'user@gmail.com'); 
+  expect(emailInput).toHaveValue('user@gmail.com'); 
 });
 
 test("checked status of checkboxes changes when user clicks them", () => {
-  // your test code here
+  render(<App />);
+  const readingCheckbox = screen.getByRole('checkbox',{name: /reading/i}); 
+
+  userEvent.click(readingCheckbox);
+  expect(readingCheckbox).toBeChecked();
+
+  userEvent.click(readingCheckbox);
+  expect(readingCheckbox).not.toBeChecked();
+
+  const watchingCheckbox = screen.getByRole('checkbox',{name: /watching/i}); 
+
+  userEvent.click(watchingCheckbox);
+  expect(watchingCheckbox).toBeChecked();
+
+  userEvent.click(watchingCheckbox);
+  expect(watchingCheckbox).not.toBeChecked();
+
+  const sleepingCheckbox = screen.getByRole('checkbox',{name: /sleeping/i});
+  
+  userEvent.click(sleepingCheckbox);
+  expect(sleepingCheckbox).toBeChecked();
+
+  userEvent.click(sleepingCheckbox);
+  expect(sleepingCheckbox).not.toBeChecked();
 });
 
 test("a message is displayed when the user clicks the Submit button", () => {
-  // your test code here
-});
+  render(<App />);
+  const submitButton = screen.getByRole('button',{name: /submit/i}); 
+  userEvent.click(submitButton);
+  expect(screen.getByText(/thank you for your subscription/i)).toBeInTheDocument();
+})
